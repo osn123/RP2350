5930099f27ba852f61a96076e6252c2efa2819e8
TakuOkubo <24CI0105@jec.ac.jp>
2025-05-17 02:02:07 +0900

    FET

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,22 @@ int ledPins2[] = {LED_PIN2};  // LED2のピン配列
 StateMachine rgb1(ledPins1, 1, 1000, 2, 2);  // LED1用のステートマシン（1秒間隔、2回繰り返し、明るさ2）
 StateMachine rgb2(ledPins2, 1, 1500, 3, 3);  // LED2用のステートマシン（1.5秒間隔、3回繰り返し、明るさ3）
 
+u32_t preMillis = 0;  // 前回のミリ秒を格納する変数
+
 // メイン関数
 void setup() {          // 初期化関数
   pinMode(LED_POWER, OUTPUT);  // LED_POWERピンを出力モードに設定
   digitalWrite(LED_POWER, HIGH);  // LEDの電源をON
+  pinMode(4, OUTPUT);  // LED_POWERピンを出力モードに設定  
+  digitalWrite(4, HIGH);  // LEDの電源をON
 }
 
 void loop() {           // メインループ関数
   rgb1.update();        // LED1のステートマシンを更新
   rgb2.update();        // LED2のステートマシンを更新
+  if (millis() - preMillis > 1000)  // 1秒ごとに実行
+  {
+   digitalWrite(4, !digitalRead(4));  // LED_POWERピンの状態を反転
+   preMillis = millis();  // 前回のミリ秒を更新 
+  }
 }
